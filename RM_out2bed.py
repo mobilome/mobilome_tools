d42de6a00942cd01f28ee77458c637f236495005
Neas <32239671+yangnaisu@users.noreply.github.com>
2019-06-01 13:58:25 +0800

    Update RM_out2bed.py

--- RM_out2bed.py
+++ RM_out2bed.py
@@ -10,23 +10,24 @@
 This script can be used to transform RepeatMasker out file into a BED file.
 """
 
-import argparse
+
+import os,argparse
+from itertools import islice 
 
 
 def main():
     parser = argparse.ArgumentParser( description='RMout_file coverter to Bed format')
-
     ## -i and -o for input/output.
     parser.add_argument('-i', '--input_file', type=str, required=True, help='Path to an input file to be read' )
     parser.add_argument('-o', '--output_file', type=str, required=True, help='Path to an output file to be created' )
-	parser.add_argument('-r', '--repeat_class', type=str, required=True, help='pick up special repeat_class' )
+    parser.add_argument('-r', '--repeat_class', type=str, required=False, help='pick up special repeat_class' )
     args = parser.parse_args()
 
     with open(args.input_file) as f:
         file = f.readlines()
     opf = open(args.output_file,'w')
     
-    for line in file:
+    for line in islice(file, 3, None):
         col = line.split()
         if len(col) > 14:
             SW_score = col[0]
@@ -44,10 +45,14 @@ def main():
             pir_end = col[12]
             pir_left = col[13]
             ID = col[14]
-            if repeat_family == args.repeat_class:
-                if strand == "C":
-                    strand = "-"
+            if strand == "C":
+                strand = "-"
+            if args.repeat_class == None:
                 opf.writelines("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\n" .format(query_sequence,piq_begin,piq_end,match_repeat,SW_score,strand))
+            else:
+                if repeat_family == args.repeat_class:
+                    opf.writelines("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\n" .format(query_sequence,piq_begin,piq_end,match_repeat,SW_score,strand))
+            
 
     opf.close()
 
